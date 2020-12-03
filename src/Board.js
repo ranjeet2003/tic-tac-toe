d3c4e756877cfde788c4c1b2800615bc7fef754c
ranjeet2003 <ranjeetgautam13032@gmail.com>
2020-12-03 20:28:55 +0530

    Adding onClick handler

--- src/Board.js
+++ src/Board.js
@@ -2,27 +2,36 @@ import React, { Component } from "react";
 import Square from "./Square";
 
 class Board extends Component {
-  render() {
-    return (
-      <div className="Board">
-        <div>
-          <Square value="X" />
-          <Square value="O" />
-          <Square value="X" />
+    state = {
+        squares: ['X', '', '', '', '', '', '', '', '']
+    }
+
+    handleClick = number => () => {
+        console.log(number)
+    }
+
+    render() {
+        const { squares } = this.state
+        return (
+            <div className="Board">
+                <div className = "Row">
+                    <Square value={ squares[0] } onClick={this.handleClick(0)} />
+                    <Square value={ squares[1] } onClick={this.handleClick(1)} />
+                    <Square value={ squares[2] } onClick={this.handleClick(2)} />
+                </div>
+                <div className = "Row">
+                    <Square value={ squares[3] } onClick={this.handleClick(3)} />
+                    <Square value={ squares[4] } onClick={this.handleClick(4)} />
+                    <Square value={ squares[5] } onClick={this.handleClick(5)} />
+                </div>
+                <div className = "Row">
+                    <Square value={ squares[6] } onClick={this.handleClick(6)} />
+                    <Square value={ squares[7] } onClick={this.handleClick(7)} />
+                    <Square value={ squares[8] } onClick={this.handleClick(8)} />
+                </div>
         </div>
-        <div>
-          <Square value="O" />
-          <Square value="X" />
-          <Square value="O" />
-        </div>
-        <div>
-          <Square value="X" />
-          <Square value="O" />
-          <Square value="X" />
-        </div>
-      </div>
-    );
-  }
+        );
+    }
 }
 
 export default Board;
--- src/Square.js
+++ src/Square.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-const Square = ({ value }) => (
-  <button className='Square' >{value}</button>
+const Square = ({ value, onClick }) => (
+  <button className='Square' onClick={onClick} >{value}</button>
 )
 
 export default Square;
